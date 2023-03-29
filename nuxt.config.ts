d025ff9c28c3ed883c1e632befde7319d1e4e5c8
cole <bernankez@qq.com>
2023-03-29 14:12:21 +0800

    chore: add i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,13 +6,45 @@ export default defineNuxtConfig({
     "@nuxt/devtools",
     "nuxt-vitest",
     "@vueuse/nuxt",
-    [
-      "@pinia/nuxt",
+    // temporary use edge version, see https://github.com/nuxt-modules/i18n/issues/1954
+    "@nuxtjs/i18n-edge",
+    "@pinia/nuxt",
+  ],
+  pinia: {
+    autoImports: [
+      "defineStore",
+    ],
+  },
+  i18n: {
+    baseUrl: "https://luyejiu.live",
+    lazy: true,
+    langDir: "lang",
+    defaultLocale: "zh",
+    experimental: {
+      jsTsFormatResource: true,
+    },
+    locales: [
       {
-        autoImports: [
-          "defineStore",
-        ],
+        code: "zh",
+        iso: "zh-CN",
+        file: "zh-CN.ts",
+        name: "简体中文",
+      },
+      {
+        code: "en",
+        iso: "en-US",
+        file: "en-US.ts",
+        name: "English",
       },
     ],
-  ],
+    strategy: "prefix_and_default",
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: "i18n_redirected",
+      redirectOn: "root",
+    },
+    vueI18n: {
+      fallbackLocale: "zh",
+    },
+  },
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,16 @@
 <template>
+  <NuxtLink v-for="loc in avaliableLocales" :key="loc.code" :to="switchLocalePath(loc.code)">
+    {{ loc.name }}
+  </NuxtLink>
+  <p>{{ $t('welcome') }}</p>
   <NuxtWelcome />
 </template>
 
 <script setup lang="ts">
+import type { LocaleObject } from "vue-i18n-routing";
 
+const { locale, locales } = useI18n();
+const switchLocalePath = useSwitchLocalePath();
+
+const avaliableLocales = computed(() => (locales.value as LocaleObject[]).filter(i => i.code !== locale.value));
 </script>
