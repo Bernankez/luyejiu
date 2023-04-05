a1bc61b9ff4865dc30b67c493ab8051f788d5999
cole <bernankez@qq.com>
2023-04-06 01:02:57 +0800

    feat(cache): rename `useSongCache` to `useHowlCache`

--- composables/cache.ts
+++ composables/cache.ts
@@ -1,17 +1,23 @@
 import type { Howl } from "howler";
 import LRU from "lru-cache";
 
-export function useSongCache<T extends { id: string; player: Howl } = { id: string; player: Howl }>(max = 10) {
-  const cache = new LRU<string, T>({
-    max,
-    dispose: (song) => {
-      song.player.unload();
-    },
-  });
+export function useHowlCache<T extends Howl = Howl>(max = 10) {
+  function beforeDelete(howl: T) {
+    howl.unload();
+  }
 
-  function add(song: T) {
-    cache.set(song.id, song);
-    return song.id;
+  const cache = max === 0
+    ? useNoneCache<string, T>({
+      dispose: beforeDelete,
+    })
+    : new LRU<string, T>({
+      max,
+      dispose: beforeDelete,
+    });
+
+  function add(id: string, howl: T) {
+    cache.set(id, howl);
+    return id;
   }
 
   function get(id: string) {
@@ -25,5 +31,31 @@ export function useSongCache<T extends { id: string; player: Howl } = { id: stri
   return {
     add,
     get,
+  };
+}
+
+export function useNoneCache<K, V>(options?: { dispose: (value: V, key: K) => void }) {
+  const cache = new Map<K, V>();
+
+  function set(key: K, value: V) {
+    cache.set(key, value);
+    return key;
+  }
+
+  function get(key: K) {
+    return cache.get(key);
+  }
+
+  function clear() {
+    for (const [key, value] of cache) {
+      options?.dispose?.(value, key);
+      cache.delete(key);
+    }
+  }
+
+  return {
+    set,
+    get,
+    clear,
   };
 }
