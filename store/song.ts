46c179542131c4dde9461910f1d8dfd45506a80d
cole <bernankez@qq.com>
2023-04-09 00:56:51 +0800

    feat(songStore):  add global states

--- store/song.ts
+++ store/song.ts
@@ -3,9 +3,15 @@ import { Playmode } from "~/types/song";
 export const useSongStore = defineStore("song", () => {
   const playmode = useLocalStorage<Playmode>(`${SONG_PREFIX}-playmode`, Playmode.sequence);
   const currentSongId = useLocalStorage(`${SONG_PREFIX}-current-song-id`, "");
+  const belongingSonglistId = useLocalStorage<string | undefined>(`${SONG_PREFIX}-belonging-songlist-id`, undefined);
+  const volume = useLocalStorage(`${SONG_PREFIX}-volume`, 1);
+  const mute = useLocalStorage(`${SONG_PREFIX}-mute`, false);
 
   return {
     playmode,
     currentSongId,
+    belongingSonglistId,
+    volume,
+    mute,
   };
 });
