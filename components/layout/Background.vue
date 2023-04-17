63d133650188ecbf66f4b13feb94e7e4b6c6918f
cole <bernankez@qq.com>
2023-04-17 19:37:53 +0800

    feat(Background): Background complete

--- components/layout/Background.vue
+++ components/layout/Background.vue
@@ -9,7 +9,7 @@
         patternTransform="rotate(-45)"
       >
         <svg x="0" y="0" :width="width" :height="height" v-html="paw" />
-        <animateTransform attributeName="patternTransform" type="translate" from="110 110" to="220 110" dur="5s" repeatCount="indefinite" additive="sum" />
+        <animateTransform attributeName="patternTransform" type="translate" from="0 0" :to="`${2 * width} 0`" dur="15s" repeatCount="indefinite" additive="sum" />
       </pattern>
       <pattern
         id="paw-reverse"
@@ -19,7 +19,7 @@
         patternTransform="rotate(-45)"
       >
         <svg :x="width" :y="1.5 * height" :width="width" :height="height" v-html="paw" />
-        <animateTransform attributeName="patternTransform" type="translate" from="220 110" to="110 110" dur="5s" repeatCount="indefinite" additive="sum" />
+        <animateTransform attributeName="patternTransform" type="translate" :from="`${2 * width} 0`" to="0 0" dur="15s" repeatCount="indefinite" additive="sum" />
       </pattern>
       <rect fill="url(#paw)" x="0" y="0" width="100%" height="100%" />
       <rect fill="url(#paw-reverse)" x="0" y="0" width="100%" height="100%" />
@@ -28,10 +28,9 @@
 </template>
 
 <script setup lang="ts">
-const { fill = "" } = defineProps<{
-  fill?: string;
-}>();
+import { orange } from "~/styles/color";
 
+const fill = ref(orange[300]);
 const ratio = 1.0619;
 const height = ref(100);
 const width = computed(() => height.value * ratio);
@@ -53,5 +52,6 @@ svg {
   z-index: -1;
   width: 100%;
   height: 100%;
+  opacity: 0.5;
 }
 </style>
