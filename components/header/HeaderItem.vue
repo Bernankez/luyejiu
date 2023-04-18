d5c978a8d0674a9b8f244aa85f326cca341c725c
cole <bernankez@qq.com>
2023-04-19 00:42:44 +0800

    feat(HeaderItem): add style for breakpoints

--- components/header/HeaderItem.vue
+++ components/header/HeaderItem.vue
@@ -1,18 +1,26 @@
 <template>
-  <NuxtLink ref="LinkRef" :to="to" :target="props.target" class="flex items-center flex-gap-1 text-gray-900 cursor-pointer select-none transition">
-    <slot name="icon" class="text-5">
-      <div :class="icon" class="text-5"></div>
-    </slot>
-    <div class="text-4 font-bold">
+  <UITooltip :disabled="sm" :delay="100">
+    <NuxtLink ref="LinkRef" :to="to" :target="props.target" class="flex items-center flex-gap-1 text-gray-900 cursor-pointer select-none transition">
+      <slot name="icon" class="text-5">
+        <div :class="icon" class="text-5"></div>
+      </slot>
+      <div class="hidden sm:block text-4 font-bold">
+        <slot>
+          {{ title }}
+        </slot>
+      </div>
+    </NuxtLink>
+    <template #content>
       <slot>
         {{ title }}
       </slot>
-    </div>
-  </NuxtLink>
+    </template>
+  </UITooltip>
 </template>
 
 <script setup lang="ts">
 import type { ComponentPublicInstance } from "vue";
+import { breakpointsTailwind } from "@vueuse/core";
 import { onActiveKey } from "./Header";
 
 const props = withDefaults(defineProps<{
@@ -20,20 +28,28 @@ const props = withDefaults(defineProps<{
   to?: string;
   target?: "_blank" | "_self" | "_parent" | "_top" | string;
   title?: string;
+  showTitle?: boolean;
 }>(), {
   icon: "",
   to: "/",
+  showTitle: undefined,
 });
 
 const onActive = inject(onActiveKey, undefined);
+const { sm } = useBreakpoints(breakpointsTailwind);
 
 const route = useRoute();
 const router = useRouter();
 const LinkRef = ref<ComponentPublicInstance>();
+const active = computed(() => router.resolve(props.to).path === route.path);
 watchEffect(() => {
-  const active = router.resolve(props.to).path === route.path;
-  if (LinkRef.value?.$el && active) {
+  if (LinkRef.value?.$el && active.value) {
     onActive?.(LinkRef.value.$el);
+  }
+});
+watch(sm, () => {
+  if (active.value) {
+    onActive?.(LinkRef.value?.$el);
   }
 });
 </script>
