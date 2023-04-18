b3053cf1e894e5a055a447fec1fa639f6e5f2907
cole <bernankez@qq.com>
2023-04-19 00:40:44 +0800

    feat(Tooltip): add `delay` prop

--- components/ui/UITooltip.vue
+++ components/ui/UITooltip.vue
@@ -11,29 +11,62 @@
 <script setup lang="ts">
 import type { Placement, Strategy } from "@floating-ui/vue";
 import { offset, useFloating } from "@floating-ui/vue";
+import type { Fn } from "@vueuse/core";
 
 const props = withDefaults(defineProps<{
   disabled?: boolean;
   strategy?: Strategy;
   placement?: Placement;
   trigger?: "click" | "hover";
+  delay?: number;
 }>(), {
   disabled: false,
   trigger: "hover",
+  delay: 0,
 });
 
-const { strategy: _strategy, placement, trigger } = toRefs(props);
+const { strategy: _strategy, placement, trigger, delay } = toRefs(props);
 
 const triggerRef = ref<HTMLDivElement>();
 const contentRef = ref<HTMLDivElement>();
 
 const showContent = ref(false);
 
+const canceled = ref(false);
+const debouncedOpenContent = ref<Fn>();
+watch(delay, (newDelay) => {
+  if (newDelay === 0) {
+    debouncedOpenContent.value = () => showContent.value = true;
+  } else {
+    debouncedOpenContent.value = useDebounceFn(() => {
+      if (canceled.value) {
+        canceled.value = false;
+        return;
+      }
+      showContent.value = true;
+    }, newDelay);
+  }
+}, {
+  immediate: true,
+});
+const openContent = () => {
+  canceled.value = false;
+  debouncedOpenContent.value?.();
+};
+const closeContent = () => {
+  showContent.value = false;
+  canceled.value = true;
+};
+
 watch(trigger, (newTrigger, _, onCleanup) => {
   if (newTrigger === "hover") {
     const { isOutside, stop } = useMouseInElement(triggerRef);
     const stopEffect = watchEffect(() => {
-      showContent.value = !isOutside.value;
+      if (isOutside.value) {
+        closeContent();
+      } else {
+        openContent();
+      }
     });
     onCleanup(() => {
       stopEffect();
@@ -43,9 +76,14 @@ watch(trigger, (newTrigger, _, onCleanup) => {
 }, {
   immediate: true,
 });
+
 const onTrigger = () => {
   if (trigger.value === "click") {
-    showContent.value = !showContent.value;
+    if (showContent.value) {
+      closeContent();
+    } else {
+      openContent();
+    }
   }
 };
 
