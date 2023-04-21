d605db58c621a09a11365187ff0747135488dd54
cole <bernankez@qq.com>
2023-04-21 17:18:38 +0800

    feat: global `Header` `PlayerBar` show state

--- app.vue
+++ app.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="grid min-h-full bg-primary-50">
     <Background />
-    <Header class="fixed top-0 left-0" />
+    <Transition name="header">
+      <Header v-if="showHeader" class="fixed top-0 left-0" />
+    </Transition>
     <main class="z-0 h-full">
       <NuxtPage />
     </main>
     <Transition name="player-bar">
-      <div v-if="contentPaddingBottom" class="fixed bottom-0 left-0 w-full">
+      <div v-if="showPlayerBar" class="fixed bottom-0 left-0 w-full">
         <PlayerBar />
       </div>
     </Transition>
@@ -14,20 +16,31 @@
 </template>
 
 <script setup lang="ts">
-const { contentPaddingTop, headerHeight, contentPaddingBottom, playerBarHeight } = storeToRefs(useAppStore());
-const mainPaddingBottom = computed(() => contentPaddingBottom.value ? playerBarHeight.value : "0");
-const mainPaddingTop = computed(() => contentPaddingTop.value ? headerHeight.value : "0");
+const { paddingBottom, paddingTop, showPlayerBar, showHeader } = storeToRefs(useAppStore());
 </script>
 
 <style scoped>
 main {
   box-sizing: border-box;
-  padding-top: v-bind(mainPaddingTop);
-  padding-bottom: v-bind(mainPaddingBottom);
+  padding-top: v-bind(paddingTop);
+  padding-bottom: v-bind(paddingBottom);
+}
+
+.header-enter-active,
+.header-leave-active {
+  /* TODO animation */
+  transition: all 0.2s ease-in;
+}
+
+.header-enter-from,
+.header-leave-to {
+  transform: translateY(-100%);
+  opacity: 0;
 }
 
 .player-bar-enter-active,
 .player-bar-leave-active {
+  /* TODO animation */
   transition: all 0.2s ease-in;
 }
 
--- store/app.ts
+++ store/app.ts
@@ -1,9 +1,17 @@
-export const useAppStore = defineStore("app", {
-  state: () => ({
-    // in most cases, pages should handle padding-top itself
-    contentPaddingTop: false,
-    headerHeight: "4rem",
-    contentPaddingBottom: true,
-    playerBarHeight: "71px",
-  }),
+export const useAppStore = defineStore("app", () => {
+  const showHeader = ref(true);
+  const showPlayerBar = ref(true);
+  const headerHeight = ref("4rem");
+  const playerBarHeight = ref("71px");
+  const paddingTop = computed(() => showHeader.value ? headerHeight.value : "0");
+  const paddingBottom = computed(() => showPlayerBar.value ? playerBarHeight.value : "0");
+
+  return {
+    showHeader,
+    showPlayerBar,
+    headerHeight,
+    playerBarHeight,
+    paddingTop,
+    paddingBottom,
+  };
 });
