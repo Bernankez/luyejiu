a7a6ed98784b93632d6942d3fb800a0a041ed2b6
cole <bernankez@qq.com>
2023-05-01 13:39:41 +0800

    chore: update test script

--- scripts/query.ts
+++ scripts/query.ts
@@ -4,13 +4,14 @@ import { run } from "./run";
 
 async function queryArtistSongs() {
   // const artist = await ArtistModel.findOne({ name: "鹿野灸" });
-  const artist = await ArtistModel.find({ name: "鹿野灸" }).populate("songs").lean();
+  const artist = await ArtistModel.find({ name: "鹿野灸" }).populate("songs");
   consola.success(artist);
   // console.log(artist[0]);
-  const songs = await SongModel.find().lean();
+  const songs = await SongModel.find().populate("artists.artist");
   // const songs1 = await SongModel.findOne();
   // songs1?.toObject();
   consola.success(songs);
+  consola.success(songs[0].toObject().artists[0]);
 }
 
 run([queryArtistSongs]);
