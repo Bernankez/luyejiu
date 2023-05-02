919565ac7e56e893f26f8eef5f217a4039314afc
cole <bernankez@qq.com>
2023-05-03 00:39:57 +0800

    chore: fix mongodb env config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -63,11 +63,11 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     // mongodb config
-    mongodbHost: process.env.MONGODB_HOST,
-    mongodbPort: process.env.MONGODB_PORT,
-    mongodbUser: process.env.MONGODB_USER,
-    mongodbPassword: process.env.MONGODB_PASSWORD,
-    mongodbDatabase: process.env.MONGODB_DATABASE,
+    mongodbHost: "",
+    mongodbPort: "",
+    mongodbUser: "",
+    mongodbPassword: "",
+    mongodbDatabase: "",
   },
   eslint: {
     emitWarning: false,
