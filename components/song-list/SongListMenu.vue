aaf586844098038d631229a01f6978e3b543e2e9
cole <bernankeic@gmail.com>
2023-06-06 14:03:07 +0800

    feat: SongListMenuItem edit

--- components/song-list/SongListMenu.vue
+++ components/song-list/SongListMenu.vue
@@ -10,11 +10,14 @@
       <SongListMenuItem icon-class="text-red-500" icon="i-solar:chat-round-like-bold" :count="11" title="我喜欢的歌曲" />
     </div>
     <div class="box-border flex flex-col rounded-2 bg-white p-2">
-      <div class="box-border p-3 p-b-1 text-5 font-bold">
+      <div class="box-border flex items-center justify-between p-3 p-b-1 text-5 font-bold">
         我的歌单
+        <div v-if="mode === 'normal'" class="i-solar:clapperboard-edit-outline" @click="() => mode = 'edit'">
+        </div>
+        <div v-if="mode === 'edit'" class="i-ic:outline-check" @click="() => mode = 'normal'"></div>
       </div>
-      <SongListMenuItem icon-class="text-primary-600" icon="i-solar:music-note-slider-2-bold" :count="4" title="全部歌曲" />
-      <SongListMenuItem icon-class="text-primary-600" icon="i-solar:music-note-slider-2-bold" :count="4" title="全部歌曲" />
+      <SongListMenuItem :editable="mode === 'edit'" icon-class="text-primary-600" icon="i-solar:music-note-slider-2-bold" :count="4" title="全部歌曲" />
+      <SongListMenuItem icon-class="text-primary-600" icon="i-solar:music-note-slider-2-bold" :count="4" title="全部歌曲全部歌曲全部歌曲全部歌曲全部歌曲全部歌曲全部歌曲全部歌曲" />
       <SongListMenuItem icon-class="text-primary-600" icon="i-solar:music-note-slider-2-bold" :count="4" title="全部歌曲" />
       <SongListMenuItem icon-class="text-primary-600" icon="i-solar:music-note-slider-2-bold" :count="4" title="全部歌曲" />
       <SongListMenuItem icon-class="text-primary-600" icon="i-solar:music-note-slider-2-bold" :count="4" title="全部歌曲" />
@@ -26,5 +29,7 @@
 </template>
 
 <script setup lang="ts">
+type Mode = "normal" | "edit";
 
+const mode = ref<Mode>("normal");
 </script>
--- components/song-list/SongListMenuItem.vue
+++ components/song-list/SongListMenuItem.vue
@@ -1,17 +1,23 @@
 <template>
-  <div class="box-border flex flex-1 cursor-default select-none items-center flex-gap-3 rounded-2 p-3 transition hover:bg-gray-50">
-    <div class="h-12 w-12 flex items-center justify-center">
-      <slot name="icon">
-        <div class="text-12" :class="[icon, iconClass]" :style="iconStyle"></div>
-      </slot>
-    </div>
-    <div class="h-full flex flex-col justify-between text-4.5">
-      {{ title }}
-      <div class="text-4 text-gray-600">
-        {{ count }}首
+  <div v-if="!showInput" class="box-border flex flex-1 cursor-default select-none items-center justify-between rounded-2 p-3 transition hover:bg-gray-50">
+    <div class="flex items-center flex-gap-3 truncate">
+      <div class="h-12 w-12 flex items-center justify-center">
+        <slot name="icon">
+          <div class="text-12" :class="[icon, iconClass]" :style="iconStyle"></div>
+        </slot>
+      </div>
+      <div class="h-full flex flex-col justify-between truncate text-4.5">
+        <div class="truncate">
+          {{ title }}
+        </div>
+        <div class="text-4 text-gray-600">
+          {{ count }}首
+        </div>
       </div>
     </div>
+    <div v-if="editable" class="i-solar:pen-2-outline shrink-0 text-5 text-gray-600" @click="() => showInput = true"></div>
   </div>
+  <SongListMenuItemInput v-else :model-value="title" @update:model-value="onEdit" @cancel="() => showInput = false" />
 </template>
 
 <script setup lang="ts">
@@ -22,6 +28,17 @@ defineProps<{
   iconClass?: string;
   iconStyle?: StyleValue;
   count?: number;
-  title?: string;
+  title?: string | number;
+  editable?: boolean;
 }>();
+
+const emit = defineEmits<{
+  (event: "edit", title: string | number): void;
+}>();
+
+const showInput = ref(false);
+
+const onEdit = (title: string | number) => {
+  emit("edit", title);
+};
 </script>
