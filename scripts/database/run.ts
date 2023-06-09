0a7fc2c66215f1f799164cfe87d5b362e22f0540
cole <bernankeic@gmail.com>
2023-06-09 16:29:22 +0800

    chore: fix script

--- scripts/database/run.ts
+++ scripts/database/run.ts
@@ -9,7 +9,7 @@ await loadConfig({
 
 export async function run(funcs: AnyFn[]) {
   const { NUXT_MONGODB_HOST = "localhost", NUXT_MONGODB_PORT = 27017, NUXT_MONGODB_DATABASE = "luyejiu", NUXT_MONGODB_USER, NUXT_MONGODB_PASSWORD, NUXT_MONGODB_SRV = false } = process.env;
-  const url = `mongodb${NUXT_MONGODB_SRV ? "+srv" : ""}://${NUXT_MONGODB_HOST}${NUXT_MONGODB_PORT ? `:${NUXT_MONGODB_PORT}` : ""}`;
+  const url = `mongodb${NUXT_MONGODB_SRV ? "+srv" : ""}://${NUXT_MONGODB_HOST}${(!NUXT_MONGODB_SRV && NUXT_MONGODB_PORT) ? `:${NUXT_MONGODB_PORT}` : ""}`;
   await mongoose.connect(url, {
     dbName: NUXT_MONGODB_DATABASE,
     user: NUXT_MONGODB_USER,
